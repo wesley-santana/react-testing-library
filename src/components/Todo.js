72506c189324dd2990de28b67c9e079b87d1b483
wesley-santana <wesleysantana_gabriel@outlook.com>
2021-08-19 19:01:53 -0300

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const INITIAL_STATE_TASK_LIST = [];
+  const [task, updateTask] = useState('');
+  const [taskList, setTaskList] = useState(INITIAL_STATE_TASK_LIST);
+  const handleClickAdd = (event) => {
+    updateTask(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setTaskList([...taskList, task]);
+    updateTask('');
+  };
+
+  const handleClickClear = () => {
+    setTaskList(INITIAL_STATE_TASK_LIST);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <input class='form-control' data-testid="input-task" placeholder="new task" value={task} onChange={handleClickAdd}/>
+        <button data-testid="add" id="add" type="subimit">Add</button>
+        <button  data-testid="clear" type="button" onClick={handleClickClear}>Clear</button>
+      </form>
+      <table data-testid="list">
+        <thead>
+            <tr>
+              <td>Tasks</td>
+            </tr>
+        </thead>
+        <tbody>
+          {taskList.map((currentTask, index) =>(
+            <tr key={index}>
+              <td>{currentTask}</td>
+            </tr>  
+          ))}
+          
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Todo;
---
+++ src/components/Todo.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {render, screen, fireEvent } from '@testing-library/react';
+import Todo from '../components/Todo';
+
+describe('Tests for Todo component', () => {
+  it('Should add new task when form has  been  submitted', () => {
+      //renderizar o componente 
+      // buscar input
+      // digitar no input
+      // buscar o botão
+      // clicar no botão
+      // buscar tabela
+      // verificar se a tarefa foi adicionada na tabela
+    
+      const { container } = render(<Todo />);
+      const input = container.querySelector('[data-testid="input-task"]');
+      fireEvent.change(input, {target: {value: 'testing'}})
+      expect(input.value).toEqual('testing');
+      const btn = container.querySelector('[data-testid="add"]');
+      fireEvent.click(btn);
+      screen.getByText('testing')
+  })
+
+  it('Remove task in list', () => {
+    const { container } = render(<Todo />);
+    const input = container.querySelector('[data-testid="input-task"]');
+    const btnAdd = container.querySelector('[data-testid="add"]');
+    const btnClear = container.querySelector('[data-testid="clear"]')
+    fireEvent.change(input, {target: {value: 'tomar água'}})
+    fireEvent.click(btnAdd);
+    fireEvent.change(input, {target: {value: 'ler um livro'}})
+    fireEvent.click(btnAdd);
+    fireEvent.click(btnClear);
+    screen.getByText('tomar água');
+  })
+})
