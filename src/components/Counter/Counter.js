294064bbea67d0fe38ba73cfc954252714ef2e03
WESLEY S GABRIEL <wesleysgabriel@Lenovo-IdeaPad-S145-15IWL-4c3d433f.localdomain>
2021-08-21 23:57:51 +0000

    feat(Counter): test class

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -4,14 +4,24 @@ import './Counter.css';
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  function getModifier() {
+    if(counter > 0) {
+      return 'Counter--success' 
+    }
+    if(counter < 0) {
+      return 'Counter--danger' 
+    }
+    return ''
+  }
+
   return (
     <>
-      <h1>{counter}</h1>
+      <h1 className={`Counter ${getModifier()}`}>{counter}</h1>
       <div>
-        <button onClick={() => setCounter(counter - 1)} className="Button">
+        <button onClick={() => setCounter(counter - 1)} className="Button Button--danger">
           -
         </button>
-        <button onClick={() => setCounter(counter + 1)} className="Button">
+        <button onClick={() => setCounter(counter + 1)} className="Button Button--success">
           +
         </button>
       </div>
--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -6,5 +6,18 @@ describe( 'Counter  Component', () => {
     render(<Counter/>);
     const counterTitle = screen.getByText('0');
     expect(counterTitle).toBeInTheDocument();
-  })
+  });
+
+  it('verify if existis Counter class in title', ()=> {
+    render(<Counter/>);
+    const counterTitle = screen.getByText('0');
+    expect(counterTitle).toHaveClass('Counter');
+  });
+
+  it('Initial value diferent of increment and decrement', ()=> {
+    render(<Counter/>);
+    const counterTitle = screen.getByText('0');
+    expect(counterTitle).not.toHaveClass('Counter--success');
+    expect(counterTitle).not.toHaveClass('Counter--danger');
+  });
 })
